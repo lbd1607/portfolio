6d6f0f450370aae36e5862ad80a7754fed8fc46d
lbd1607 <35612977+lbd1607@users.noreply.github.com>
2022-08-06 16:17:54 -0500

    Fix brand position on mobile

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -1,26 +1,19 @@
 import React from "react";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/router";
 
 export default function Navbar() {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
-  const router = useRouter();
+
   return (
-    <nav className="mobile-navbar lg:navbar  flex flex-wrap items-center justify-between  p-3 lg:bg-transparent lg:p-10 text-bonewhite fixed">
+    <nav
+      className={
+        "mobile-navbar lg:navbar flex flex-wrap items-center justify-between bg-primary-medium p-3 lg:bg-transparent lg:p-10 text-bonewhite fixed"
+      }
+    >
       <div className="container flex flex-wrap items-center justify-between px-4">
         <div className="relative flex w-full justify-between lg:static lg:block lg:w-auto lg:justify-start">
-          <h3 className="flex cursor-pointer items-center text-2xl ">
-            <Link href={"/"}>
-              <div className="contents font-semibold">
-                Laura Davis
-                <span className="flex px-2"></span>
-              </div>
-            </Link>
-          </h3>{" "}
           <button
             className="block cursor-pointer rounded border border-solid border-transparent bg-transparent px-3 py-1 text-xl leading-none  outline-none focus:outline-none lg:hidden"
             type="button"
@@ -29,13 +22,26 @@ export default function Navbar() {
             <FontAwesomeIcon icon={"bars"} size="sm" />
           </button>
         </div>
+
         <div
           className={
             "flex-grow items-center lg:flex" +
-            (navbarOpen ? " flex" : " hidden")
+            (navbarOpen ? "block py-5" : " hidden")
           }
         >
-          <ul className="flex list-none flex-col  uppercase  lg:absolute  lg:right-0 lg:ml-auto lg:flex-row lg:px-16 mt-3 lg:mt-0">
+          <h3 className="cursor-pointer items-center text-2xl lg:ml-0 lg:pb-0 ml-3 lg:border-none border-b-2 pb-3 border-accent">
+            <Link href={"/"}>
+              <div className="contents font-semibold">
+                Laura Davis
+                <span className="flex px-2"></span>
+              </div>
+            </Link>
+          </h3>{" "}
+          <ul
+            className={
+              "flex list-none flex-col  uppercase  lg:absolute  lg:right-0 lg:ml-auto lg:flex-row lg:px-16 mt-3 lg:mt-0"
+            }
+          >
             <li className="lg:px-2 py-1">
               <Link href={"#projects"}>Projects</Link>
             </li>{" "}
